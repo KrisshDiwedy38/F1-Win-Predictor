a9e970d8cca597d2b4491deb322564222a5eb15a
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-04-04 19:55:22 +0530

    Connecting to postgreSQL

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from matplotlib import pyplot as plt
-import fastf1
-import fastf1.plotting
+import fastf1 as f1
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
-fastf1.plotting.setup_mpl(misc_mpl_mods=False, color_scheme='fastf1')
+db_params = {
+    "dbname": "first",
+    "user": "postgres",
+    "password": "Krissh",
+    "host": "localhost",
+    "port": "5432"
+}
 
-session = fastf1.get_session(2019, 'Monza', 'Q')
+conn_str = f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}"
 
+# Create SQLAlchemy engine
+engine = create_engine(conn_str)
+
+# Test connection
+with engine.connect() as conn:
+    print("Connected to PostgreSQL!")
+
+f1.Cache.enable_cache("F1_Cache")
+values = []
+session = f1.get_session(2024,2,'R')
 session.load()
-print(session.json())
+print((session.weather_data).to_string())
+# print(session.results.columns)
+# race_data = session.results.loc[:,['TeamName','Abbreviation','FullName','Time', 'Position']].copy()
+# time = 0
+# for row in race_data.sort_values("Position").itertuples():
+#    time += (row.Time).total_seconds()
+#    data = session
+#    data = {
+#       'Position' : row.Position,
+#       'TeamName' : row.TeamName,
+#       'DriverCode' : row.Abbreviation,
+#       'FullName' : row.FullName,
+#       'Time' : round(time,5)
+#    }
+
+#    values.append(data)
+
+# df = pd.DataFrame(values)
+
+# df.to_sql("racedata", engine, if_exists="append", index=False)
+# print("Data inserted successfully!")
