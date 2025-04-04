71debcd8d1213e349677eb858ee80bfb031918e4
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-04-04 23:25:32 +0530

    Updating

--- app.py
+++ app.py
@@ -22,26 +22,8 @@ with engine.connect() as conn:
 
 f1.Cache.enable_cache("F1_Cache")
 values = []
-session = f1.get_session(2024,2,'R')
-session.load()
-print((session.weather_data).to_string())
-# print(session.results.columns)
-# race_data = session.results.loc[:,['TeamName','Abbreviation','FullName','Time', 'Position']].copy()
-# time = 0
-# for row in race_data.sort_values("Position").itertuples():
-#    time += (row.Time).total_seconds()
-#    data = session
-#    data = {
-#       'Position' : row.Position,
-#       'TeamName' : row.TeamName,
-#       'DriverCode' : row.Abbreviation,
-#       'FullName' : row.FullName,
-#       'Time' : round(time,5)
-#    }
-
-#    values.append(data)
-
-# df = pd.DataFrame(values)
-
-# df.to_sql("racedata", engine, if_exists="append", index=False)
-# print("Data inserted successfully!")
+session = f1.get_session(2025,2,'R')
+session._load_weather_data()
+session._load_drivers_results()
+print(session.results.columns)
+print(session.weather_data["Rainfall"])
