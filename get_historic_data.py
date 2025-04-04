ff98f81c47f6bd975022267fad401ad18af1338c
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-04-04 19:56:09 +0530

    Filtering data from API for Database and model

--- get_historic_data.py
+++ get_historic_data.py
@@ -1,13 +1,17 @@
-import fastf1
+import fastf1 as f1
+import pandas as pd 
+
+f1.Cache.enable_cache("F1_Cache")
 
 years = [2022, 2023, 2024]
-events = ['Q','R']
+events = ['R']
 rounds = [22,22,24]
 
-count = -1 
+count = -1
 for year in years:
    count += 1
-   for round in range(1,rounds[count]+1):
-      for event in events:
-         session = fastf1.get_session(year, round, event)
-         print(session)
+   for event in events:
+      for round in range(1,rounds[count] + 1):
+         session = f1.get_session(year, round, event)
+         name = str(session).split(":")[1].split("-")[0].strip()
+         print(f"{year} - {name}")
