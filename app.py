1e66b58dd6eca802d9471393094eeb6c24b2b782
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-04-09 20:46:29 +0530

    Adding Comments

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from datetime import date
 from driver_performance import driver_performance_data 
 from driver_performance import driver_mean_df
 
-f1.Cache.enable_cache("F1_Cache")
-
+# SQL Accessing parameters
 db_params = {
     "dbname": "f1data",
     "user": "postgres",
@@ -29,13 +28,19 @@ engine = create_engine(conn_str)
 with engine.connect() as conn:
     print("Connected to PostgreSQL!")
 
+# Getting the Performance and LapTime of drivers on the specified circuit for the last 3 years
 def get_circuit_performance(df, circuit):
+
+    # Circuit data columns - RaceName, Performance, DriverCode for the specified circuit
     circuit_data = df[df["RaceName"] ==  circuit].loc[: , ["RaceName","Performance","DriverCode"]].copy()
+
+    # Lap Time data columns - DriverCode, LapTime for the specified circuit
     lap_time_data = df[df["RaceName"] == circuit].loc[:,["DriverCode","LapTime"]].copy()
-    circuit = circuit.replace(" ","")
+    circuit = circuit.replace(" ","") # Remove spaces from circuit name
     performance_list = []
     time_list = []
 
+    # Calculating mean performance and mean lap time for drivers on the circuit
     for driver in circuit_data['DriverCode'].unique():
         mean_perf = circuit_data[circuit_data['DriverCode'] == driver]['Performance'].mean()
         performance_list.append({
@@ -52,50 +57,62 @@ def get_circuit_performance(df, circuit):
     lap_time_df = pd.DataFrame(time_list)
     return circuit_df , lap_time_df
 
-
+# Extract dataset from SQL to IDE in form of a pandas dataframe 
 df = pd.read_sql('SELECT * FROM "F1ResultData" ORDER BY "index" ;', engine)
 weather_df = pd.read_sql('SELECT * FROM "F1WeatherData" ORDER BY "index" ;', engine)
 
-
 driver_data = df.loc[:,["DriverCode", "Time"]].copy()
 
+# Get Qualifying data for the race predictions
 quali_data = {
     'DriverCode': [
-        'VER', 'PER', 'HAM', 'RUS', 'LEC', 'SAI', 'NOR', 'PIA', 'ALO', 'STR',
-        'OCO', 'GAS', 'ALB', 'SAR', 'BOT', 'ZHO', 'MAG', 'HUL', 'RIC', 'TSU'
+        'NOR', 'VER', 'RUS', 'HAM', 'PIA', 'LEC', 'SAI', 'PER', 'ALO', 'HUL',
+        'STR', 'ALB', 'MAG', 'GAS', 'RIC', 'TSU', 'OCO', 'ZHO', 'BOT', 'SAR'
     ],
     'QualiTime': [
-        87.312, 87.579, 87.842, 87.925, 88.003, 88.127, 88.210, 88.305, 88.412, 88.630,
-        88.794, 88.851, 89.003, 89.215, 89.384, 89.442, 89.603, 89.705, 89.822, 90.001
+        101.243, 101.355, 101.427, 101.558, 101.697, 101.800, 101.887, 101.922, 102.019, 102.055,
+        102.213, 102.368, 102.507, 102.513, 102.777, 102.797, 102.857, 103.193, 103.220, 103.379
     ]
 }
 
 quali_df = pd.DataFrame(quali_data)
 
-circuit = "Japanese Grand Prix"
+circuit = "Singapore Grand Prix"
 
+# Getting circuit performance and lap times
 circuit_performance , Lap_time_df = get_circuit_performance(driver_performance_data, circuit) 
 
 results_df = quali_df.copy()
 
+# Merge the needed databases for training model
 merged_df = pd.merge(quali_df,driver_mean_df, on='DriverCode', how='inner')
 merged_df = pd.merge(merged_df,circuit_performance, on='DriverCode', how='inner')
 merged_df = pd.merge(merged_df,Lap_time_df, on='DriverCode', how='inner')
 
+# Define X and y
 X = merged_df[['QualiTime', 'MeanPerformance', 'RacePerformance']]
 y = merged_df['LapTime']
 
+# Split the data into Train and Test sets 
 X_train, X_test, y_train, y_test = train_test_split(X , y , test_size = 0.2, random_state = 39)
+
+# Training Gradient Boosting model for predicting the winner
 model = GradientBoostingRegressor(n_estimators = 100, learning_rate = 0.1, random_state = 39)
 model.fit(X_train, y_train)
 
+# Predicting
 predicted_lap_times = model.predict(merged_df[['QualiTime', 'MeanPerformance', 'RacePerformance']])
 results_df["PredictedRaceTime"] = predicted_lap_times
 
-results_df = results_df.sort_values(by="PredictedRaceTime")
+results_df = results_df.sort_values("PredictedRaceTime")
 
-print(results_df)
+# Output
+print(f"F1 {circuit} Prediction:")
+print(f"Predicted Winner: {results_df.iloc[0]['DriverCode']}")
+print("Top 3 Predictions")
+top3= results_df[['DriverCode', 'PredictedRaceTime']].head(3)
+print(top3[["DriverCode","PredictedRaceTime"]].to_string(index=False))
 
+# Calculating the Mean Squared Error
 y_pred = model.predict(X_test)
-print("R² Score:", r2_score(y_test, y_pred))
-print("RMSE:", np.sqrt(mean_squared_error(y_test, y_pred)))
+print("Root Mean Squared Error:", np.sqrt(mean_squared_error(y_test, y_pred)))
